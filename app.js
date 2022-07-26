ebfe0e6b1e434c325284aed8c40661e7627aa3ca
aravindneeluri <aravindreddy.neeluri@gmail.com>
2022-07-26 16:01:11 +0530

    added dots

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ mongoose.connect('mongodb://127.0.0.1:27017/arvind',{useNewUrlParser: true, useU
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error: "));
 db.once("open", function () {
-  console.log("Connected successfully");
+  console.log("Connected successfully...");
 });
 
 //EJS
